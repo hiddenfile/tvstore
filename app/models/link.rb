09db6d61fb2753b6c830aa775326c9fbfb7ff1b4
Max Dolgih <maxim.dolgih@gmail.com>
2015-03-10 03:59:12 +0200

    fast commits

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,26 @@
 class Link < ActiveRecord::Base
   LIMIT = 10.freeze
 
+  validate :validate_url
+
   scope :limit_order, -> (u) { where(user_id: u.id).order('created_at desc').limit(Link::LIMIT)}
+
+  def short_date
+    created_at.to_s(:short)
+  end
+
+  def short_url
+    uri = URI.parse(url)
+    uri.path.gsub('/', '').blank? ? uri.host : uri.path.truncate(50)
+  end
+
+  private
+
+  def validate_url
+    begin
+      URI.parse(url)
+    rescue URI::InvalidURIError
+      errors.add(:base, 'The format of the url is not valid.')
+    end
+  end
 end
